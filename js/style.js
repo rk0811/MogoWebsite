1423961db1233a9ce98309e0f0cc41fe20c48fc2
Rakesh Goud <rakeshkalal2001@gmail.com>
2019-06-07 02:45:01 +0530

    Initial Commit

---
+++ js/style.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+
+$('.nino-testimonialSlider').unslider({
+		nav: false,
+		arrows: {
+			//  Unslider default behaviour
+			prev: '<a class="unslider-arrow prev"><i class="mdi mdi-chevron-left"></i></a>',
+			next: '<a class="unslider-arrow next"><i class="mdi mdi-chevron-right"></i></a>',
+		}
+	});
